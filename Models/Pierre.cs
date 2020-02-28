a9e6db8f561bf83c5ebb2f8575096ffeaf0f5524
DominiqueYoumans <ddyoumans@icloud.com>
2020-02-28 08:40:34 -0800

    add seperate logic and two classes

---
+++ Models/Pierre.cs
@@ -0,0 +1,43 @@
+using System;
+
+namespace Pierre.Models
+{
+
+  public class Bread
+  {
+    public string Name { get; set; }
+    public string Type { get; set; }
+    public int Variety { get; set; }
+
+
+    public Bread()
+    {
+
+      Name = name;
+      Type = type;
+      Variety = variety;
+
+    }
+
+  }
+
+  public class Pastry
+  {
+    public string Name { get; set; }
+    public string Type { get; set; }
+    public string Variety  { get; set; }
+
+
+
+
+    public Pastry()
+    {
+      Name = name;
+      Type = type;
+      Variety = variety; 
+
+
+
+    }
+  }
+}
