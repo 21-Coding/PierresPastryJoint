4b530d7c5f69240c20c22009508c33dacc1a6f5b
DominiqueYoumans <ddyoumans@icloud.com>
2020-02-28 11:51:03 -0800

    add updates

--- Program.cs
+++ Program.cs
@@ -1,51 +1,54 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using Pierre.Models;
 
 namespace Pierre
 {
-    public class Program
+  public class Program
+  {
+    public static void Main()
     {
-        public static void Main()
-        {
-          Bread englishMuff = new Bread("English Muffin","Blueberry",5);
-          Bread bagel = new Bread("Bagel","Onion",5);
-          Bread rolls = new Bread("Roll","Honey",5);
+      Bread englishMuff = new Bread("English Muffin","Blueberry",5);
+      Bread bagel = new Bread("Bagel","Onion",5);
+      Bread rolls = new Bread("Roll","Honey",5);
 
-          List<Bread> Bread = new List<Bread>() {englishMuff, bagel, rolls};
+      List<Bread> Bread = new List<Bread>() {englishMuff, bagel, rolls};
+      foreach(Bread style in Bread)
+      {
+        Console.WriteLine(style);
+      }
 
+      Pastry croissant = new Pastry("Croissant","Plain",5);
+      Pastry cheeseDanish = new Pastry("Danish","Cheese",5);
+      Pastry cheeseCakeCup = new Pastry("Cheesecake Cup","Strawberry",5);
 
-          Pastry croissant = new Pastry("Croissant","Plain",5);
-          Pastry cheeseDanish = new Pastry("Danish","Cheese",5);
-          Pastry cheeseCakeCup = new Pastry("Cheesecake Cup","Strawberry",5);
-
-          List<Pastry> Pastry = new List<Pastry>() {croissant, cheeseDanish, cheeseCakeCup};
-          Console.WriteLine("Welcome to Pierres Pastry Joint! Our bread is $5.00 & a pastry is only $2.00.");
-
-
-          Console.WriteLine("To select a bread enter 1.");
-          string stringBread = Console.ReadLine();
-          int intBread = int.Parse(stringBread);
-
-          if (stringBread == "1")
-          {
-            Bread.ForEach(Console.WriteLine);
-          }
-
-          Console.WriteLine("To select a pastry enter 2.");
-          string stringPastry = Console.ReadLine();
-          int intPastry = int.Parse(stringPastry);
-
-          if (stringPastry == "2")
-          {
-            Pastry.ForEach(Console.WriteLine);
-          }
+      List<Pastry> Pastry = new List<Pastry>() {croissant, cheeseDanish, cheeseCakeCup};
+      Pastry.ForEach(Console.WriteLine);
 
 
+      Console.WriteLine("Welcome to Pierres Pastry Joint! Our bread is $5.00 & a pastry is only $2.00.");
 
 
+      Console.WriteLine("To select a bread enter 1.");
+      string stringBread = Console.ReadLine();
+      int intBread = int.Parse(stringBread);
+
+      if (stringBread == "1")
+      {
+
+       Console.WriteLine(Bread);
 
 
-        }
+      }
+
+      Console.WriteLine("To select a pastry enter 2.");
+      string stringPastry = Console.ReadLine();
+      int intPastry = int.Parse(stringPastry);
+
+      if (stringPastry == "2")
+      {
+
+      }
     }
+  }
 }
