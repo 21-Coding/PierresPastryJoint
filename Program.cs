19f8a70476d1d905c44cb47c3801a6c29374696b
DominiqueYoumans <ddyoumans@icloud.com>
2020-02-28 14:44:43 -0800

    add new logic

--- Program.cs
+++ Program.cs
@@ -2,49 +2,37 @@ using System;
 using System.Collections.Generic;
 using Pierre.Models;
 
-namespace Pierre
-{
+
   public class Program
   {
     public static void Main()
     {
-      Console.WriteLine("Welcome to Pierres Pastry Joint! Our bread is $5.00 & a pastry is only $2.00");
-      Console.WriteLine("We have the following breads:  ");
-      Bread englishMuff = new Bread("English Muffin","Blueberry",5);
-      Bread bagel = new Bread("Bagel","Onion",5);
-      Bread rolls = new Bread("Roll","Honey",5);
+      Bread freshBread = new Bread();
+      Pastry freshPastry = new Pastry();
 
-      List<Bread> Bread = new List<Bread>() {englishMuff, bagel, rolls};
-      Bread.ForEach(Console.WriteLine);
+      Console.WriteLine("Pierres Pasty Joint");
+      Console.WriteLine("We offer bread by the loaf & pastries as well.  Press 1 for bread & 2 for pastries");
+      string userSpeak = Console.ReadLine();
 
-      Console.WriteLine("We have the following pastries:  ");
-
-      Pastry croissant = new Pastry("Croissant","Plain",5);
-      Pastry cheeseDanish = new Pastry("Danish","Cheese",5);
-      Pastry cheeseCakeCup = new Pastry("Cheesecake Cup","Strawberry",5);
-
-      List<Pastry> Pastry = new List<Pastry>() {croissant, cheeseDanish, cheeseCakeCup};
-      Pastry.ForEach(Console.WriteLine);
-
-
-
-
-      Console.WriteLine("To select a bread [enter 1]  To select a pastry [enter 2]");
-      string stringBread = Console.ReadLine();
-      int intBread = int.Parse(stringBread);
-
-      if (stringBread == "1")
+      if (userSpeak == 1)
       {
-        Console.WriteLine(Bread[0]);
+        Console.Write("Please enter the number of loaves for purchase:  ");
+        string loafCount = Console.ReadLine();
+        int intLoafCount = int.Parse(loafCount);
       }
-      else if (stringBread == "2")
+      else if (userSpeak == 2)
       {
-        Console.WriteLine(Pastry);
+        Console.WriteLine("Please enter the number of pastries for purchase:  ");
+        string pastryCount = Console.ReadLine();
+        int intPastryCount = int.Parse(pastryCount);
       }
       else
       {
+        Console.WriteLine("Please enter 1 or 2.");
+        Main();
+      }
 
       }
+
     }
   }
-}
