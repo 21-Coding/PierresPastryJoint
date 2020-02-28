01dc5d82a7d04713d202902d36e90af23435adeb
DominiqueYoumans <ddyoumans@icloud.com>
2020-02-28 10:35:35 -0800

    add pastry list and write lines for both classes.

--- Program.cs
+++ Program.cs
@@ -13,7 +13,27 @@ namespace Pierre
           Bread rolls = new Bread("Roll","Honey",5);
 
           List<Bread> Bread = new List<Bread>() {englishMuff, bagel, rolls};
-          Console.WriteLine("Welcome to Pierres Pastry Joint!");
+          Console.WriteLine("Welcome to Pierres Pastry Joint! Our bread is $5.00 & a pastry is only $2.00.");
+
+          Pastry croissant = new Pastry("Croissant","Plain",5);
+          Pastry cheeseDanish = new Pastry("Danish","Cheese",5);
+          Pastry cheeseCakeCup = new Pastry("Cheesecake Cup","Strawberry",5);
+
+          List<Pastry> Pastry = new List<Pastry>() {croissant, cheeseDanish, cheeseCakeCup};
+          Console.WriteLine("Welcome to Pierres Pastry Joint! Our bread is $5.00 & a pastry is only $2.00.");
+
+
+          Console.WriteLine("To select a bread enter 1.");
+          string stringBread = Console.ReadLine();
+          int intBread = int.Parse(stringBread);
+
+          Console.WriteLine("To select a pastry enter 2.");
+          string stringPastry = Console.ReadLine();
+          int intPastry = int.Parse(stringPastry);
+
+
+
+
 
 
         }
