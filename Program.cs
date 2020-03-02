a64e030bb24894ac1d6d3bc7864d6311dbc8746e
DominiqueYoumans <ddyoumans@icloud.com>
2020-03-01 22:50:49 -0800

    add sleep method to control the flow of text into the terminal

--- Program.cs
+++ Program.cs
@@ -1,43 +1,64 @@
-using System;
-using Pierre.Models;
+  using System;
+  using Pierre.Models;
 
-namespace Pierre
-{
-  public class Program
+  namespace Pierre
   {
-    public static void Main()
+    public class Program
     {
-      Console.WriteLine("Thank You for shopping at Pierre's Pastry Joint!");
-      Console.Write("Please provide a name for the order: ");
-      string orderName = Console.ReadLine();
+      public static void QuickText(string line)
+      {
+        for (int i = 0; i < line.Length; i++)
+          {
+            Console.Write(line[i]);
+            System.Threading.Thread.Sleep(50);
+          }
+      }
+      public static void Main()
+      {
+        Console.ForegroundColor = ConsoleColor.Blue;
+        Console.BackgroundColor = ConsoleColor.Black;
+        QuickText("Thank You for shopping at Pierre's Pastry Joint!");
+        System.Threading.Thread.Sleep(2000);
+        Console.Clear();
+        QuickText("Please provide a name for the order: ");
+        string orderName = Console.ReadLine();
 
-      Console.WriteLine("Todays bread special is:  ");
-      Console.WriteLine("Blueberry Hard Dough Bread.");
-      Console.WriteLine("$5 per loaf or buy 2, get one on us!");
-      Console.WriteLine("How many loaves would you like?");
-
-
-      int breadCount = int.Parse(Console.ReadLine());
-      Bread freshLoaf = new Bread(breadCount);
-      int breadAccount = freshLoaf.BreadCheckout(breadCount);
-
-      Console.WriteLine("Todays pastry special is:  ");
-      Console.WriteLine("Strawberry Cheesecake Cups.");
-      Console.WriteLine("$2 per pastry or $5 for 3!");
-
-      Console.WriteLine("How many pastries would you like?");
-
-      int pastryCount = int.Parse(Console.ReadLine());
-      Pastry freshOne = new Pastry(pastryCount);
-      int pastryAccount = freshOne.PastryCheckout(pastryCount);
-
-      Console.Clear();
-      Console.WriteLine();
-      Console.WriteLine("Thank you " + orderName + " Your total order is: ");
-      Console.WriteLine("$" + breadAccount + pastryAccount + ".00");
+        Console.ForegroundColor = ConsoleColor.White;
+        QuickText("Todays bread special is:  " + Environment.NewLine);
+        Console.ForegroundColor = ConsoleColor.White;
+        QuickText("Blueberry Hard Dough Bread." + Environment.NewLine);
+        Console.ForegroundColor = ConsoleColor.White;
+        QuickText("$5 per loaf or buy 2, get one on us!" + Environment.NewLine);
+        Console.ForegroundColor = ConsoleColor.Blue;
+        QuickText("How many loaves would you like?" + Environment.NewLine);
 
 
+        int breadCount = int.Parse(Console.ReadLine());
+        Bread freshLoaf = new Bread(breadCount);
+        int breadAccount = freshLoaf.BreadCheckout(breadCount);
+        Console.Clear();
 
+        Console.ForegroundColor = ConsoleColor.White;
+        QuickText("Todays pastry special is:  " + Environment.NewLine);
+        Console.ForegroundColor = ConsoleColor.White;
+        QuickText("Strawberry Cheesecake Cups." + Environment.NewLine);
+        Console.ForegroundColor = ConsoleColor.White;
+        QuickText("$2 per pastry or $5 for 3!" + Environment.NewLine);
+        Console.ForegroundColor = ConsoleColor.Blue;
+        QuickText("How many pastries would you like?");
+
+        int pastryCount = int.Parse(Console.ReadLine());
+        Pastry freshOne = new Pastry(pastryCount);
+        int pastryAccount = freshOne.PastryCheckout(pastryCount);
+
+        Console.Clear();
+        Console.ForegroundColor = ConsoleColor.Cyan;
+        Console.BackgroundColor = ConsoleColor.Black;
+        QuickText("Thank you " + orderName + " your total order is: " + Environment.NewLine);
+        QuickText("$" + breadAccount + pastryAccount + ".00");
+
+
+
+      }
     }
   }
-}
